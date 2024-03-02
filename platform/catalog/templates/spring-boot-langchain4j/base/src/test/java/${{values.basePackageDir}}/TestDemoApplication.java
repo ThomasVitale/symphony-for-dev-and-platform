049e60f16df5c5760967ef06b3ec76f950871bc7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-03-02 22:19:24 +0100

    Add templates section

---
+++ platform/catalog/templates/spring-boot-langchain4j/base/src/test/java/${{values.basePackageDir}}/TestDemoApplication.java
@@ -0,0 +1,53 @@
+package ${{ values.basePackage }};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+{% if values.llmProvider == "chroma" %}
+import org.testcontainers.chromadb.ChromaDBContainer;
+{% endif %}
+{% if values.llmProvider == "weaviate" %}
+import org.testcontainers.weaviate.WeaviateContainer;
+{% endif %}
+
+{% if values.llmProvider == "ollama" %}
+import io.thomasvitale.langchain4j.testcontainers.service.containers.OllamaContainer;
+{% endif %}
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDemoApplication {
+
+	{% if values.llmProvider == "ollama" %}
+	@Bean
+	@RestartScope
+	@ServiceConnection
+	OllamaContainer ollama() {
+		return new OllamaContainer("ghcr.io/thomasvitale/ollama-llama2");
+	}
+	{% endif %}
+
+	{% if values.llmProvider == "chroma" %}
+	@Bean
+	@RestartScope
+	@ServiceConnection
+	ChromaDBContainer chroma() {
+		return new ChromaDBContainer("ghcr.io/chroma-core/chroma:0.4.24");
+	}
+	{% endif %}
+
+	{% if values.llmProvider == "weaviate" %}
+	@Bean
+	@RestartScope
+	@ServiceConnection
+	WeaviateContainer weaviate() {
+		return new WeaviateContainer("semitechnologies/weaviate:1.23.10");
+	}
+	{% endif %}
+
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+	}
+
+}
