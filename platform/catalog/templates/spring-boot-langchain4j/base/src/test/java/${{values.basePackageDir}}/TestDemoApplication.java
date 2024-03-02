e9c5ef1207e611866168c492396b79b82f67cfb3
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-03-03 00:10:30 +0100

    Update catalog

--- platform/catalog/templates/spring-boot-langchain4j/base/src/test/java/${{values.basePackageDir}}/TestDemoApplication.java
+++ platform/catalog/templates/spring-boot-langchain4j/base/src/test/java/${{values.basePackageDir}}/TestDemoApplication.java
@@ -5,46 +5,46 @@ import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-{% if values.llmProvider == "chroma" %}
+{%- if values.vectorStore == "chroma" %}
 import org.testcontainers.chromadb.ChromaDBContainer;
-{% endif %}
-{% if values.llmProvider == "weaviate" %}
+{%- endif %}
+{%- if values.vectorStore == "weaviate" %}
 import org.testcontainers.weaviate.WeaviateContainer;
-{% endif %}
+{%- endif %}
 
-{% if values.llmProvider == "ollama" %}
+{%- if values.llmProvider == "ollama" %}
 import io.thomasvitale.langchain4j.testcontainers.service.containers.OllamaContainer;
-{% endif %}
+{%-endif %}
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestDemoApplication {
 
-	{% if values.llmProvider == "ollama" %}
+	{%-if values.llmProvider == "ollama" %}
 	@Bean
 	@RestartScope
 	@ServiceConnection
 	OllamaContainer ollama() {
 		return new OllamaContainer("ghcr.io/thomasvitale/ollama-llama2");
 	}
-	{% endif %}
+	{%- endif %}
 
-	{% if values.vectorStore == "chroma" %}
+	{%- if values.vectorStore == "chroma" %}
 	@Bean
 	@RestartScope
 	@ServiceConnection
 	ChromaDBContainer chroma() {
 		return new ChromaDBContainer("ghcr.io/chroma-core/chroma:0.4.24");
 	}
-	{% endif %}
+	{%- endif %}
 
-	{% if values.vectorStore == "weaviate" %}
+	{%- if values.vectorStore == "weaviate" %}
 	@Bean
 	@RestartScope
 	@ServiceConnection
 	WeaviateContainer weaviate() {
 		return new WeaviateContainer("semitechnologies/weaviate:1.23.10");
 	}
-	{% endif %}
+	{%- endif %}
 
 	public static void main(String[] args) {
 		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
