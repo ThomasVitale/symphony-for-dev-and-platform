a31c830f2e1a22d34d39521f7d6daa969dffbc50
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-03-03 09:26:33 +0100

    Update catalog

--- platform/catalog/templates/spring-boot-langchain4j/base/src/test/java/${{values.basePackageDir}}/DemoApplicationTests.java
+++ platform/catalog/templates/spring-boot-langchain4j/base/src/test/java/${{values.basePackageDir}}/DemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-{% if values.llmProvider == "openai" %}
+{%- if values.llmProvider == "openai" %}
 @EnabledIfEnvironmentVariable(named = "LANGCHAIN4J_OPENAI_CLIENT_API_KEY", matches = ".*")
 {%- endif %}
 @SpringBootTest
